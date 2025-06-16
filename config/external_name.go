4381bb38d39b2cb247a62b78902361db559156c5
VALKIRIA ACUATICA <56233573+valkiriaaquatica@users.noreply.github.com>
2025-06-16 14:04:40 +0200

    update the linting

--- config/external_name.go
+++ config/external_name.go
@@ -27,9 +27,9 @@ var ExternalNameConfigs = map[string]config.ExternalName{
 	"proxmox_virtual_environment_file":                            config.IdentifierFromProvider,
 	"proxmox_virtual_environment_firewall_alias":                  config.IdentifierFromProvider,
 	"proxmox_virtual_environment_firewall_ipset":                  config.IdentifierFromProvider,
-	"proxmox_virtual_environment_firewall_options": 			   config.IdentifierFromProvider,
-	"proxmox_virtual_environment_group": 						   config.IdentifierFromProvider,
-	"proxmox_virtual_environment_hagroup":						   config.IdentifierFromProvider,
+	"proxmox_virtual_environment_firewall_options": 	       config.IdentifierFromProvider,
+	"proxmox_virtual_environment_group": 			       config.IdentifierFromProvider,
+	"proxmox_virtual_environment_hagroup":			       config.IdentifierFromProvider,
 	"proxmox_virtual_environment_firewall_rules":                  config.IdentifierFromProvider,
 	"proxmox_virtual_environment_metrics_server":                  config.IdentifierFromProvider,
 	"proxmox_virtual_environment_network_linux_bridge":            config.IdentifierFromProvider,
@@ -37,8 +37,8 @@ var ExternalNameConfigs = map[string]config.ExternalName{
 	"proxmox_virtual_environment_pool":                            config.IdentifierFromProvider,
 	"proxmox_virtual_environment_time":                            config.IdentifierFromProvider,
 	"proxmox_virtual_environment_vm":                              config.IdentifierFromProvider,
-	"proxmox_virtual_environment_haresource": 					   config.IdentifierFromProvider,
-	"proxmox_virtual_environment_hosts":							   config.IdentifierFromProvider,
+	"proxmox_virtual_environment_haresource": 		       config.IdentifierFromProvider,
+	"proxmox_virtual_environment_hosts":			       config.IdentifierFromProvider,
 }
 
 // ExternalNameConfigurations applies all external name configs listed in the
